887bc996f0d03d9bb04e923dd46044144b1f9d7e
charlesonunze <charlesonunze2@gmail.com>
2020-12-04 21:22:57 +0100

    add use case and refactor

--- 04-Arrays-and-Slices/arrays_test.go
+++ 04-Arrays-and-Slices/arrays_test.go
@@ -16,15 +16,20 @@ func Test(t *testing.T) {
 		}
 	})
 
+	checkSums := func(t *testing.T, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("want %d but got %d given", want, got)
+		}
+	}
+
 	t.Run("sum all", func(t *testing.T) {
 		input1 := []int{1, 2}
 		input2 := []int{3, 4}
 		got := SumAll(input1, input2)
 		want := []int{3, 7}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %d but got %d given %v and %v", want, got, input1, input2)
-		}
+		checkSums(t, got, want)
 	})
 
 	t.Run("sum all tails", func(t *testing.T) {
@@ -33,8 +38,15 @@ func Test(t *testing.T) {
 		got := SumAllTails(input1, input2)
 		want := []int{5, 9}
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("want %d but got %d given %v and %v", want, got, input1, input2)
-		}
+		checkSums(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		input1 := []int{}
+		input2 := []int{3, 4, 5}
+		got := SumAllTails(input1, input2)
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
 	})
 }
