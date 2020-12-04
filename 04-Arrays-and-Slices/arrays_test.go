aba1156d826d6ef135ab0f8a43d2df14830a4ce1
charlesonunze <charlesonunze2@gmail.com>
2020-12-04 19:12:30 +0100

    dont repeat yourself

--- 04-Arrays-and-Slices/arrays_test.go
+++ 04-Arrays-and-Slices/arrays_test.go
@@ -12,14 +12,4 @@ func Test(t *testing.T) {
 			t.Errorf("want %d but got %d given %v", want, got, input)
 		}
 	})
-
-	t.Run("collection of any size", func(t *testing.T) {
-		input := []int{1, 2, 3}
-		got := Sum(input)
-		want := 6
-
-		if got != want {
-			t.Errorf("want %d but got %d given %v", want, got, input)
-		}
-	})
 }
