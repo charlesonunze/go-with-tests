d99da0bfb956e8e5ec7b770163e1ca8837776f09
charlesonunze <charlesonunze2@gmail.com>
2020-12-04 19:04:17 +0100

    use slices

--- 04-Arrays-and-Slices/arrays_test.go
+++ 04-Arrays-and-Slices/arrays_test.go
@@ -3,11 +3,23 @@ package slices
 import "testing"
 
 func Test(t *testing.T) {
-	input := [5]int{1, 2, 3, 4, 5}
-	got := Sum(input)
-	want := 15
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		input := []int{1, 2, 3, 4, 5}
+		got := Sum(input)
+		want := 15
 
-	if got != want {
-		t.Errorf("want %d but got %d given %v", want, got, input)
-	}
+		if got != want {
+			t.Errorf("want %d but got %d given %v", want, got, input)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		input := []int{1, 2, 3}
+		got := Sum(input)
+		want := 6
+
+		if got != want {
+			t.Errorf("want %d but got %d given %v", want, got, input)
+		}
+	})
 }
