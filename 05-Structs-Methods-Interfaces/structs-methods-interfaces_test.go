b0d88dcc0bbf8d2964f407c3a0b4ba612efaa978
charlesonunze <charlesonunze2@gmail.com>
2020-12-05 11:10:21 +0100

    table driven tests

--- 05-Structs-Methods-Interfaces/structs-methods-interfaces_test.go
+++ 05-Structs-Methods-Interfaces/structs-methods-interfaces_test.go
@@ -3,24 +3,18 @@ package interfaces
 import "testing"
 
 func Test(t *testing.T) {
-	checkArea := func(t *testing.T, s Shape, want float64) {
-		t.Helper()
-		got := s.Area()
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{Rectangle{2.0, 3.0}, 6.0},
+		{Circle{2.0}, 12.566370614359172},
 	}
 
-	t.Run("calculate area of a rectangle", func(t *testing.T) {
-		rectangle := Rectangle{2.0, 3.0}
-		want := 6.0
-		checkArea(t, rectangle, want)
-	})
-
-	t.Run("calculate area of a circle", func(t *testing.T) {
-		circle := Circle{2.0}
-		want := 12.566370614359172
-		checkArea(t, circle, want)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
