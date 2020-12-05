65c3c6bfe23d3c451731c1ed536798be95691dd9
charlesonunze <charlesonunze2@gmail.com>
2020-12-05 11:23:48 +0100

    refactor

--- 05-Structs-Methods-Interfaces/structs-methods-interfaces_test.go
+++ 05-Structs-Methods-Interfaces/structs-methods-interfaces_test.go
@@ -2,21 +2,24 @@ package interfaces
 
 import "testing"
 
-func Test(t *testing.T) {
+func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		want  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-		{Square{2.0, 2.0}, 4.0},
-		{Triangle{4.0, 5.0}, 10.0},
-		{Rectangle{2.0, 3.0}, 6.0},
-		{Circle{2.0}, 12.566370614359172},
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
 	}
 
 	for _, tt := range areaTests {
-		got := tt.shape.Area()
-		if got != tt.want {
-			t.Errorf("got %g want %g", got, tt.want)
-		}
+		// using tt.name from the case to use it as the `t.Run` test name
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
+		})
 	}
 }
